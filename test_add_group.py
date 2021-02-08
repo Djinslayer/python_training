d23bed345b1167b2ae0e0538a048b9c03bacc33c
Nikita Byuller <Djinslayer@gmail.com>
2021-02-08 17:14:44 +0300

    Шаблон проектирования, создание группы параметров

--- test_add_group.py
+++ test_add_group.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 import unittest
+from group import Group
+
+def is_alert_present(wd):
+    try:
+        wd.switch_to_alert().text
+        return True
+    except:
+        return False
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
@@ -22,19 +30,19 @@ class TestAddGroup(unittest.TestCase):
     def open_group_page(self, wd):
         wd.find_element_by_link_text("groups").click()
 
-    def create_group(self, wd, name, header, footer):
+    def create_group(self, wd, group):
         # создание новой группы
         wd.find_element_by_name("new").click()
         # заполнение формы
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(name)
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(header)
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(footer)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         # нажатие кнопки создание группы
         wd.find_element_by_name("submit").click()
 
@@ -49,7 +57,7 @@ class TestAddGroup(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_group_page(wd)
-        self.create_group(wd, name="Group1", header="Test 08.02.21", footer="test 08.02.21")
+        self.create_group(wd, Group(name="Group1", header="Test 08.02.21", footer="test 08.02.21"))
         self.return_to_groups_page(wd)
         self.logout(wd)
 
@@ -58,25 +66,12 @@ class TestAddGroup(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_group_page(wd)
-        self.create_group(wd, name="", header="", footer="")
+        self.create_group(wd, Group(name="", header="", footer=""))
         self.return_to_groups_page(wd)
         self.logout(wd)
 
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-
     def tearDown(self):
         self.wd.quit()
-
 
 if __name__ == "__main__":
     unittest.main()
