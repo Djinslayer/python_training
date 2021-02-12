8677bec36950bf57457c558e76930f9136790115
Nikita Byuller <Djinslayer@gmail.com>
2021-02-12 19:18:27 +0300

    Создан класс для работы с группами и контактами

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,7 +13,7 @@ def app(request):
 
 def test_add_contact(app):
         app.session.login(username="admin", password="secret")
-        app.create_contact(Contact(firstname="Anonimus", middlename="Anonim", lastname="Anonomovich",
+        app.contact.create_contact(Contact(firstname="Anonimus", middlename="Anonim", lastname="Anonomovich",
                             nickname="Anonim_nick", tittle="test", company="MTS", address_company="Kazan",
                             home_phone="+7999999999", mobile_phone="+7888888888", work_phone="+7562562562",
                             fax="+7235235689", mail1="email@mail1.ru", mail2="email@mail2.ru", mail3="email@mail3.ru",
@@ -24,7 +24,7 @@ def test_add_contact(app):
 
 def test_add_empty_contact(app):
         app.session.login(username="admin", password="secret")
-        app.create_contact(Contact(firstname="", middlename="", lastname="",
+        app.contact.create_contact(Contact(firstname="", middlename="", lastname="",
                                    nickname="", tittle="", company="", address_company="",
                                    home_phone="", mobile_phone="", work_phone="",
                                    fax="", mail1="", mail2="", mail3="",
