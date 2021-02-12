57e72d53be6eedfcdcf6c23a51837e1def81acc8
Nikita Byuller <Djinslayer@gmail.com>
2021-02-12 18:58:33 +0300

    Создан класс для управления сессиями

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,22 +12,22 @@ def app(request):
 
 
 def test_add_contact(app):
-        app.login(username="admin", password="secret")
+        app.session.login(username="admin", password="secret")
         app.create_contact(Contact(firstname="Anonimus", middlename="Anonim", lastname="Anonomovich",
                             nickname="Anonim_nick", tittle="test", company="MTS", address_company="Kazan",
                             home_phone="+7999999999", mobile_phone="+7888888888", work_phone="+7562562562",
                             fax="+7235235689", mail1="email@mail1.ru", mail2="email@mail2.ru", mail3="email@mail3.ru",
                             homepage="qxl-ex.ru", birthday_year="1990", anniversary_year="2010", address2="Kazan 2",
                             phone2="Home", notes="Notes"))
-        app.logout()
+        app.session.logout()
 
 
 def test_add_empty_contact(app):
-        app.login(username="admin", password="secret")
+        app.session.login(username="admin", password="secret")
         app.create_contact(Contact(firstname="", middlename="", lastname="",
                                    nickname="", tittle="", company="", address_company="",
                                    home_phone="", mobile_phone="", work_phone="",
                                    fax="", mail1="", mail2="", mail3="",
                                    homepage="", birthday_year="", anniversary_year="", address2="",
                                    phone2="", notes=""))
-        app.logout()
+        app.session.logout()
