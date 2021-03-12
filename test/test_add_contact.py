6b169e1acb862347c830b96682cef45cc0a51b05
Nikita Byuller <Djinslayer@gmail.com>
2021-03-12 16:44:17 +0300

    Отделение тестовых данных от сценариев

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,31 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
 import pytest
-import random
-import string
-
-def random_string(prefix, maxlen):
-        symbols = string.ascii_letters + string.digits + ' '*10
-        return prefix + ''.join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-#random_day = randint(1, 31)
-
-
-testdata = [Contact(firstname="", middlename="", lastname="", nickname="", tittle="", company="", address="",
-                    home_phone="", mobile_phone="", work_phone="", fax="", mail1="", mail2="", mail3="",
-                    homepage="", birthday_year="", anniversary_year="", address2="", phone2="", notes="")] + [
-        Contact(firstname=random_string('firstname', 15), middlename=random_string('middlename', 20),
-                lastname=random_string('lastname', 30), nickname=random_string('nickname', 30),
-                tittle=random_string('tittle', 20), company=random_string('company', 30),
-                address=random_string('address', 30), home_phone=random_string('home_phone', 12),
-                mobile_phone=random_string('mobile_phone', 12), work_phone=random_string('work_phone', 12),
-                fax=random_string('fax', 12), mail1=random_string('mail1', 30), mail2=random_string('mail2', 30),
-                mail3=random_string('mail3', 30), homepage=random_string('homepage', 30),
-                bday='9', bmonth='November', birthday_year='1990', aday="20", amonth="April", anniversary_year="2010",
-                address2=random_string('address2', 12), phone2=random_string('phone2', 12),
-                notes=random_string('notes', 12))
-        for i in range(5)
-]
+from data.add_contact import constant as testdata
 
 
 @pytest.mark.parametrize('contact', testdata, ids=[repr(x) for x in testdata])
