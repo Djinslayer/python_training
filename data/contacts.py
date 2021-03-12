b90fc549e5d7aa6545b8e093a2ecee2a8b015247
Nikita Byuller <Djinslayer@gmail.com>
2021-03-12 21:40:27 +0300

    динамическая генерация тестов

---
+++ data/contacts.py
@@ -0,0 +1,16 @@
+from model.contact import Contact
+
+
+testdata = [
+    Contact(firstname=('firstname'), middlename=('middlename'), lastname=('lastname'), nickname=('nickname'),
+            tittle=('tittle'), company=('company'), address=('address'), home_phone=('home_phone'),
+            mobile_phone=('mobile_phone'), work_phone=('work_phone'), fax=('fax'), mail1=('mail1'), mail2=('mail2'),
+            mail3=('mail3'), homepage=('homepage'), bday='9', bmonth='November', birthday_year='1990',
+            aday="20", amonth="April", anniversary_year="2010", address2=('address2'), phone2=('phone2'), notes=('notes')),
+    Contact(firstname=('firstname1'), middlename=('middlename1'), lastname=('lastname1'), nickname=('nickname1'),
+            tittle=('tittle1'), company=('company1'), address=('address1'), home_phone=('home_phone1'),
+            mobile_phone=('mobile_phone1'), work_phone=('work_phone1'), fax=('fax1'), mail1=('mail11'), mail2=('mail21'),
+            mail3=('mail31'), homepage=('homepage1'), bday='9', bmonth='November', birthday_year='1990',
+            aday="20", amonth="April", anniversary_year="2010", address2=('address21'), phone2=('phone21'),
+            notes=('notes1'))
+]
