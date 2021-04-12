53fe3871c2572c03ab5f8ac5036c1ef88dd32ca2
Nikita Byuller <Djinslayer@gmail.com>
2021-04-12 18:18:13 +0300

    настройка Jenkins

--- bdd/group_steps.py
+++ bdd/group_steps.py
@@ -5,7 +5,7 @@ from model.group import Group
 def group_list(db):
     return db.get_group_list()
 
-@given('a group with <name>, <header> and <footer>')
+@given('a group with <name>, <header> and <footer>',  target_fixture="new_group")
 def new_group(name, header, footer):
     return Group(name=name, header=header, footer=footer)
 
